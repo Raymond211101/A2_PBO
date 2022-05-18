74f0506c0a7c3f0bcd0220ef399d96297a15bec3
Raymond Jonathan Damanik <raymondjonathan2101@gmail.com>
2022-05-18 20:33:01 +0800

    2009106057_RaymmondJonathanDamanik_POSTTEST6

---
+++ 2009106057_RaymondJonathanDamanik/src/posttest6/Login.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package posttest6;
+import java.io.IOException;
+
+
+public interface Login {
+    void masuk()throws IOException;
+    void keluar()throws IOException;
+}
